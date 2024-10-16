2f6df1bbd6519cf05604e465dba719aeaa4e53d7
deproger <vl.ebel.tm@gmail.com>
2024-10-16 16:31:18 +0700

    removed sanity

--- sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import category from "./category";
-import footer from "./footer";
-import heroImages from "./heroImages";
-import product from "./product";
-import privacy from "./privacy";
-
-export const schemaTypes = [heroImages, product, category, privacy, footer]
